35ae6236808dc267f3ca1022c84a8050d453f6cb
code-for-coffee <jtraverjr@gmail.com>
2016-05-21 20:22:56 -0500

    'james stop nerding it up, it is a saturday' - george

--- app/src/main/java/com/countryquiz/codeforcoffee/countryquiz/QuizActivity.java
+++ app/src/main/java/com/countryquiz/codeforcoffee/countryquiz/QuizActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
